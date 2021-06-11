6584740db395b7377706a10f17132beb5da742f6
Hery Andrianjafinimanana <36274996+Herynyandry@users.noreply.github.com>
2021-06-11 11:55:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const final = document.querySelector('.output');
+
+// run the calculation on minute and hour keyup
+[hours, minutes].forEach(input => input.addEventListener('keyup', () => {
+  const hval = parseFloat(hours.value) || 0;
+  const mval = parseFloat(minutes.value) || 0;
+  const result = (hval + mval / 60).toFixed(2);
+  final.value = result;
+}));
